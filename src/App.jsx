61474ac7d6e618a836781bc48ba83fcddb7798ea
Puviarasu <puviarasu.se2024cse@sece.ac.in>
2025-12-03 09:30:27 +0530

    Adding the Task1 Files

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+//import HelloWorld from "./hello-world/hello-world-component";
+
+// function App() {
+//   return (
+//     <>
+//       <HelloWorld />
+//       {/* Removed the extra hello world */}
+//     </>
+//   );
+// }
+
+// export default App;
+import Banner from "./banner";
+import "./banner.css";
+
+function App() {
+  return (
+    <>
+      <Banner />
+    </>
+  );
+}
+
+export default App;
---
+++ src/banner.jsx
@@ -0,0 +1,13 @@
+function Banner() {
+  return (
+    <div className="banner">
+      <h2>Learners Today, Leaders Tomorrow</h2>
+      <p>
+        With our continuous research and development, we provide <br />
+        you with an excellent Aptitude training.
+      </p>
+    </div>
+  );
+}
+
+export default Banner;
