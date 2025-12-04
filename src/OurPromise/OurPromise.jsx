0ea9ae5762c4ec6728588fff8058fcf6c9d9dc36
Puviarasu <puviarasu.se2024cse@sece.ac.in>
2025-12-04 22:02:49 +0530

    Updated the practice tasks

--- src/OurPromise/OurPromise.jsx
+++ src/OurPromise/OurPromise.jsx
@@ -7,7 +7,8 @@ const OurPromise = () => {
       <div className="promise-container">
         <h2 className="Title">Our Promise</h2>
         <p className="para">
-          As part of our high-quality service, we'd like to offer something extra too.
+          As part of our high-quality service, we'd like to offer something
+          extra too.
         </p>
       </div>
 
@@ -26,9 +27,9 @@ const OurPromise = () => {
         <div className="element-item1">
           <h5>Responsive</h5>
           <p>
-            We understand the value of a positive attitude, timely responsiveness,
-            and adaptability. We're dedicated to attentively listening and leveraging
-            our utmost capabilities to serve you.
+            We understand the value of a positive attitude, timely
+            responsiveness, and adaptability. We're dedicated to attentively
+            listening and leveraging our utmost capabilities to serve you.
           </p>
         </div>
 
@@ -36,9 +37,9 @@ const OurPromise = () => {
         <div className="element-item2">
           <h5>Rockstar Team</h5>
           <p>
-            Our expertise is our unique selling proposition. You'll have access to an
-            all-star team of experienced professionals fully dedicated to serving you,
-            armed with coding skills that are truly exceptional.
+            Our expertise is our unique selling proposition. You'll have access
+            to an all-star team of experienced professionals fully dedicated to
+            serving you, armed with coding skills that are truly exceptional.
           </p>
         </div>
 
@@ -47,8 +48,8 @@ const OurPromise = () => {
           <h5>Fast Turn-arounds</h5>
           <p>
             The importance of speed is on par with quality for us. We are swift,
-            nimble, and capable of providing high-quality code within a timeframe
-            that suits your preferences.
+            nimble, and capable of providing high-quality code within a
+            timeframe that suits your preferences.
           </p>
         </div>
 
@@ -56,9 +57,9 @@ const OurPromise = () => {
         <div className="element-item4">
           <h5>Life-time support</h5>
           <p>
-            Our commitment to code quality is exemplified by our enduring support. If
-            any bugs happen to emerge in the future, feel free to reach out to us for
-            assistance.
+            Our commitment to code quality is exemplified by our enduring
+            support. If any bugs happen to emerge in the future, feel free to
+            reach out to us for assistance.
           </p>
         </div>
 
@@ -66,9 +67,9 @@ const OurPromise = () => {
         <div className="element-item5">
           <h5>Secured Agreement</h5>
           <p>
-            The creative work you receive is entirely your own, and we unequivocally
-            respect that. We assure you of our commitment and readiness to formalize
-            this with a Non-Disclosure Agreement (NDA).
+            The creative work you receive is entirely your own, and we
+            unequivocally respect that. We assure you of our commitment and
+            readiness to formalize this with a Non-Disclosure Agreement (NDA).
           </p>
         </div>
       </div>
